f029caa7f41e5e18289b6cf43d7560ede0de7b71
martinstoo <martin.stoyanov@bbzbl-it.ch>
2024-06-06 16:26:40 +0200

    dev and prod + started with classes

---
+++ dev/rules/rule.py
@@ -0,0 +1,32 @@
+import os, requests, json
+from dotenv import load_dotenv
+
+load_dotenv()
+RULES_ENDPOINT = os.getenv('rules_url')
+
+#TODO add error handling for e.g. for network errors + validation
+class Rule:
+    @staticmethod
+    def getRulesList():
+        rules_list = []
+        try:
+            response = requests.get(RULES_ENDPOINT)
+            response.raise_for_status()
+            data = response.json()
+            for item in data:
+                rules_list.append(item)
+        except requests.RequestException as e:
+            print(f"Network error: {e}")
+        except ValueError as e:
+            print(f"JSON decode error: {e}")
+        return rules_list
+
+    def ListOfRules(self):
+        rules_list = self.getRulesList()
+        for rule in rules_list:
+            rule['rules_id']
+            print(rule)
+
+# can only return 1 rule
+rule_instance = Rule()
+rule_instance.ListOfRules()
---
+++ dev/rules/service.py
@@ -0,0 +1,45 @@
+import os, requests, json
+from dotenv import load_dotenv
+
+load_dotenv()
+RULES_ENDPOINT = os.getenv('rules_url')
+
+
+class Service():
+    def waterService():
+        print("water")
+
+    def schedulingService():
+        print("schedule")
+
+    def sensorService():
+        print("sensors")
+
+
+
+
+# class Rule:
+#   def getSchedule():
+#   def execute():
+
+# Create a rule object  via static method  on the Rule class, does validation
+# rule = Rule.createFrom(ruleJSON);
+
+# if rule.isEnabled()
+#   addJob(rule);
+
+
+# some other file...
+# addJob(rule):
+#   mySchedulingApi.scheduleTask(rule.getSchedule().toCtron())
+    # scheduler calls rule.execute();
+
+# rule.execute::
+# - check conditiations against rule? ranges of temp/humidity, etc.
+# - if in range, waterService.water(rule.getDuration())
+# current_zone_temperature = sensorService.getTemperature(rule.getZoneID())
+# current_zone_rh = sensorService.getHunidity(rule.getZoneID())
+# current_oat = sensorService.getOutsideAirTemperature()
+# current_orh = sensorService.getOutsideRelativeHumidity()
+# if (....)
+#   waterService.water(rule.getDuration())
